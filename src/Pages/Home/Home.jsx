5797ccb309775e899f4632ac68966dc084817052
josueaorozco <113638774+josueaorozco@users.noreply.github.com>
2025-12-21 12:13:57 -0500

    fix/contact

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Home() {
   return (
     <div className="page">
-      <h1>Lingua e Arti d’Italia</h1>
-      <p>Where cultures meet and create.</p>
+      <h1>Lingua e arti d’Italia</h1>
+      <p>Dove le culture si incontrano e creano</p>
     </div>
   );
 }
